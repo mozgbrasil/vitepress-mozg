df1bb09c739e34c7974dffb07a068521317d91c4
mozgbrasil <mozgbrasil@gmail.com>
2025-02-06 03:06:51 +0000

    Deploying to gh-pages from @ mozgbrasil/vitepress-mozg@57ce629a28975c8408b4c8533db0fccad6a87eb5 🚀

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
+class MyApp {
+  constructor() {
+    console.log('MyApp inicializado!')
+  }
+
+  executeMethod(methodName) {
+    if (methodName === 'Calc') {
+      this.Calc()
+    } else {
+      console.error(`Método ${methodName} não encontrado.`)
+    }
+  }
+
+  Calc() {
+    console.log('Executando Calc!')
+  }
+}
+
+// Expor a classe globalmente
+window.MyApp = MyApp
+
+//
+
+const isShadowRootModeSupported =
+  HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode')
+
+// alert(isShadowRootModeSupported)
+
+//
+
 function getIn(status) {
   console.log({ name: 'getIn ', status, url: window.location.href })
+
+  new window.MyApp().executeMethod('Calc')
+
+  // const isShadowRootModeSupported =
+  //   HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode')
+
+  // document
+  //   .querySelector('p[hidden]')
+  //   .toggleAttribute('hidden', isShadowRootModeSupported)
 }
 
 if (document.readyState === 'complete') {
